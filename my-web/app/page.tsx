2be6dca27346ec51ab867e48532eb433f59e2ea2
Akila Senanayake <akilasenanayake22@gmail.com>
2025-02-15 14:31:23 +0530

    Test navigate routes

--- my-web/app/page.tsx
+++ my-web/app/page.tsx
@@ -1,8 +1,22 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
+    <>
     <h1>Home page</h1>
+    <br></br>
+
+    <ul>
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/service">Services</Link></li>
+      <li><Link href="/products">Products</Link></li>
+      <li><Link href="/about/branches">About</Link></li>
+      <li>Contact</li>
+      <li>Login</li>
+    </ul>
+    </>
+    
   );
 }
